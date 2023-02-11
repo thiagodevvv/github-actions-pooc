9354dbd74e22613c33e53e0c1f01f63cad3efa6e
Thiago <th.ferrari@outlook.com.br>
2023-02-11 07:44:01 -0300

    test reports

---
+++ test/index.js
@@ -0,0 +1,54 @@
+
+const assert = require("assert")
+
+async function functionPromisy() {
+
+}
+
+describe("#suite de testes", () => {
+    it("#teste 1", async () => {
+
+        async function functionPromisy() {
+            return new Promise((resolve) => {
+                setTimeout(() => {
+                    resolve(true)
+                }, 10000)
+            })
+        }
+        const response = await functionPromisy()
+        assert.ok(response)
+    })
+    it("#teste 2", async () => {
+        async function functionPromisy() {
+            return new Promise((resolve) => {
+                setTimeout(() => {
+                    resolve(true)
+                }, 15000)
+            })
+        }
+        const response = await functionPromisy()
+        assert.ok(response)
+    })
+    it("#teste 3", async () => {
+        async function functionPromisy() {
+            return new Promise((resolve) => {
+                setTimeout(() => {
+                    resolve(true)
+                }, 20000)
+            })
+        }
+        const response = await functionPromisy()
+        assert.ok(response)
+    })
+    it("#teste 4", async () => {
+        async function functionPromisy() {
+            return new Promise((resolve) => {
+                setTimeout(() => {
+                    resolve(true)
+                }, 10000)
+            })
+        }
+        const response = await functionPromisy()
+        assert.ok(response)
+    })
+})
